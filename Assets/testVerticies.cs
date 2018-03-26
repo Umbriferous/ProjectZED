437e4417b70bffa90e26c4ed90318c9a627b4fb7
Nik Shulga <nikiknik@gmail.com>
2018-03-26 14:27:05 +0200

    Test Verticies

---
+++ Assets/testVerticies.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testVerticies : MonoBehaviour {
+
+    Color[] colors;
+    Mesh mesh;
+    Vector3[] vertices;
+
+    private void Start() {
+        mesh = GetComponent<MeshFilter>().mesh;
+        vertices = mesh.vertices;
+        colors = new Color[vertices.Length];
+    }
+
+    void Update() {
+        int i = 0;
+        while (i < vertices.Length) {
+            colors[i] = new Color(1, 1, 0);
+            i++;
+        }
+        mesh.colors = colors;
+    }
+}
